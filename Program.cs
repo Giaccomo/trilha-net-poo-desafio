6577c89cf5d13f757243903cf1b49116e90a19aa
Giaccomo <giaccomo@outlook.com.br>
2024-07-21 16:25:18 -0300

    Envio desafio 3

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456", "Modelo Prem","154654654444",64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+Console.WriteLine("");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Nokia("987654", "Modelo Regu","777774444",256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegran");
+
